724091a8e23f7a72c9833dd5ebb8fc163bb088a8
johntista85 <johntista85@gmail.com>
2023-02-18 02:10:32 -0800

    7-print_tebahpla.c commit mgs

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,12 @@
 int main(void)
 
 {
-
-        char low;
-
-        for (low = 'z'; low >= 'a'; low--)
-        putchar(low);
-        putchar('\n');
-
-        return (0);
+	
+	char low;
+	
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+	
+	return (0);
 }
