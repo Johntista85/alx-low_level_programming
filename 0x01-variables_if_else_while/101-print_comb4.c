12c1819db7186f30f335d3b5b36962f729989cfd
johntista85 <johntista85@gmail.com>
2023-02-18 05:12:54 -0800

    101-print_comb4.c commit mgs

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 					putchar(d);
 					putchar(p);
 					putchar(q);
-					if (q == '7' && p == '8')
+					if (d == '7' && p == '8')
 						continue;
 					putchar(',');
 					putchar(' ');
