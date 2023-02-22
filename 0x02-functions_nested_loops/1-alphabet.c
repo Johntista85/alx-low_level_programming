a9f0b870021a6b121d3a69d380e2f3c9910e3b2d
johntista85 <johntista85@gmail.com>
2023-02-21 20:58:55 -0800

    1-alphabet.c commit mgs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,12 @@
-#include "main"
+#include "main.h"
 
-void print_alphabet(void);
+/**
+ * main - main function
+ * _putchar - print the alphabate
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
 {
 	char j;
 
