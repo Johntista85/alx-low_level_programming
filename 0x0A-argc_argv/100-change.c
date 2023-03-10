dd069f3327ee35ca3e95f8cf7d496ecb510c0492
johntista85 <johntista85@gmail.com>
2023-03-10 06:26:48 -0800

    100-change.c mgs

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,7 +13,6 @@
 int main(int argc, char *argv[])
 {
 	int num, j, result;
-
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
@@ -29,7 +28,6 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
