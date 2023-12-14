c3245cf3b9711fbc00ba16a22c034058dae7d508
Johntista85 <johntista85.com>
2023-12-13 22:22:03 -0800

    Updates

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t j = 0;
+	size_t i = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	for (; j < size; j++)
+	for (; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
-		if (array[j] == value)
-			return (j);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	} return (-1);
 }
